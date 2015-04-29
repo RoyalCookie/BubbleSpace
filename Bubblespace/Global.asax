ed844ce152766ef867c93dfc1ee7ebca8ddd2870
Andri Rafn <andrirafna@gmail.com>
2015-04-29 15:47:17 +0000

    Initial Push

---
+++ Bubblespace/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bubblespace.MvcApplication" Language="C#" %>
---
+++ Bubblespace/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bubblespace.Startup))]
+namespace Bubblespace
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
