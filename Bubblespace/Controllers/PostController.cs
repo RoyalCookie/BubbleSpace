b46d0324831ce7210eea8ea5b05e27e2678a3132
RoyalCookie <janusthorkristjansson@gmail.com>
2015-05-09 21:00:11 +0000

    View HTML breyting og Post Controller

--- Bubblespace/Controllers/PostController.cs
+++ Bubblespace/Controllers/PostController.cs
@@ -26,30 +26,37 @@ namespace Bubblespace.Controllers
             AspNetUsers user = (from a in dbUsers
                        where a.Email == Convert.ToString(identity)
                        select a).Single();
-            
-            
-            /*
+
             posts postToInsert = new posts();
             postToInsert.content_text = collection["content_text"];
-            postToInsert.content_is_video = Convert.ToByte(collection["content_is_video"]);
-            postToInsert.time_inserted = time;
+            postToInsert.content_is_video = Convert.ToByte(0);
+
+            
+            postToInsert.time_inserted = DateTime.Now;
+            // Real Below In Comments - Above For Testing
+            //postToInsert.time_inserted = Convert.ToDateTime(collection["time"]);
+           
             postToInsert.FK_posts_users = user.Id;
-            postToInsert.FK_posts_bubble_groups = bubbleGroup;
-             */
+
+            
+            postToInsert.FK_posts_bubble_groups = null;
+            // Real Below Using Null Currently As Testing
+            //postToInsert.FK_posts_bubble_groups = Convert.ToInt32(collection["FK_posts_bubble_groups"]);
 
 
-
-
+            db.posts.Add(postToInsert);
+            db.SaveChanges();
+            
             // Debug Info
             System.Diagnostics.Debug.WriteLine("You've Called Post.Create()");
-            System.Diagnostics.Debug.WriteLine("Current Identity:");
-            System.Diagnostics.Debug.WriteLine("Email: " + user.Email);
-            System.Diagnostics.Debug.WriteLine("Nickname: " + user.NickName);
-            System.Diagnostics.Debug.WriteLine("Id: " + user.Id);
+            System.Diagnostics.Debug.WriteLine("Current Data:");
+            System.Diagnostics.Debug.WriteLine("Content: " + postToInsert.content_text);
+            System.Diagnostics.Debug.WriteLine("User: " + user.Email);
+            System.Diagnostics.Debug.WriteLine("UserId: " + user.Id);
 
 
 
-            return Json("{'Jonas':1}");
+            return Json("asd");
         }
 
         public ActionResult LikePost()
