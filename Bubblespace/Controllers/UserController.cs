f170814dfcf54e861b6f6899cff183be437d6c10
Andri Rafn Ágústsson <andrirafna@gmail.com>
2015-05-09 19:38:53 +0000

    Meira ajax.

--- Bubblespace/Controllers/UserController.cs
+++ Bubblespace/Controllers/UserController.cs
@@ -55,13 +55,20 @@ namespace Bubblespace.Controllers
             return View();
         }
 
+        [HttpPost]
         public ActionResult Friends()
         {
             // For now this returns all users, not just friends.
             // We need to change this to accept an id and return only friends. - Andri
             var db = new VERK2015_H17Entities1();
             var allUsers = db.AspNetUsers.ToList();
-            return Json(allUsers);
+            var usernames = ( from user in allUsers select user.NickName).ToList();
+            var images = (from user in allUsers select user.profile_image).ToList();
+
+            List<List<string>> returnJson = new List<List<string>>();
+            returnJson.Add(usernames);
+            returnJson.Add(images);
+            return Json(returnJson);
         }
 	}
 }
