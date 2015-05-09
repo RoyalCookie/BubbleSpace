1288ce764d323be322d50d485849e8f9cb57549d
RoyalCookie <janusthorkristjansson@gmail.com>
2015-05-09 20:08:57 +0000

    PostController

--- Bubblespace/Controllers/PostController.cs
+++ Bubblespace/Controllers/PostController.cs
@@ -10,14 +10,48 @@ namespace Bubblespace.Controllers
     {
         //
         // GET: /Post/
-        public ActionResult Index()
+        [HttpPost]
+        public ActionResult Create(string postContent, string contentPicture, DateTime time, int bubbleGroup, byte isVideo)
         {
-            return View();
-        }
+            
+            // Get The Currently Logged In Users Email
+            string identity = User.Identity.Name;
 
-        public ActionResult Create()
-        {
-            return View();
+            // Create A  Database Entity
+            var db = new VERK2015_H17Entities1();
+            
+            // Get a List Of All Users
+            var dbUsers = db.AspNetUsers.ToList();
+            
+            // Get The User Data
+            AspNetUsers user = (from a in dbUsers
+                       where a.Email == Convert.ToString(identity)
+                       select a).Single();
+            
+            
+            posts postToInsert = new posts();
+
+            postToInsert.content_text = postContent;
+            postToInsert.content_is_video = isVideo;
+            postToInsert.content_picture = contentPicture;
+            postToInsert.time_inserted = time;
+            postToInsert.FK_posts_users = user.Id;
+            postToInsert.FK_posts_bubble_groups = bubbleGroup;
+
+            db.posts.Add(postToInsert);
+
+
+
+            // Debug Info
+            System.Diagnostics.Debug.WriteLine("You've Called Post.Create()");
+            System.Diagnostics.Debug.WriteLine("Current Identity:");
+            System.Diagnostics.Debug.WriteLine("Email: " + user.Email);
+            System.Diagnostics.Debug.WriteLine("Nickname: " + user.NickName);
+            System.Diagnostics.Debug.WriteLine("Id: " + user.Id);
+
+
+
+            return Json("{'Jonas':1}");
         }
 
         public ActionResult LikePost()
