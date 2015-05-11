4828eb56c076768c4d54de2f55eff75876f47825
Andri Rafn Ágústsson <andrirafna@gmail.com>
2015-05-11 16:54:41 +0000

    Wall works! :dancers:

--- Bubblespace/Scripts/ajax.js
+++ Bubblespace/Scripts/ajax.js
@@ -2,36 +2,45 @@
     var friendslist = $("#list-view-items");
     friendslist.empty();
     $.post("/User/Friends", function (data) {
+        console.log(data);
         for (var i = 0; i < data[0].length; i++) {
+            
             friendslist.append(
                   "<li class='list-item'>"
                 + "<img src='/Content/Assets/" + data[1][i] + ".png'/>"
-                + "<a onclick='friendMain("+ data[2][i] +"); return false;'>" + data[0][i] + "</a></li>"
+                + "<a onclick='friendMain(\""+ data[2][i] +"\"); return false;' id='user-name' data-val='" + data[2][i] + "'>" + data[0][i] + "</a></li>"
             );
         }
     })
 }
 
 function friendMain(id) {
-    var mainView = $("#main-view");
-    mainView.empty();
-    $.post("/User/GetUserInformation?" + id, function (posts) {
-        // TODO: Header
-        for (var i = posts[0].length - 1; i >= 0; i--) {
-            mainView.append(
-                    "<li class='feed-post'>"
-                  + "<img class='post-profile-image' src='/Content/Assets/" + posts[2][i] + ".png' />"
-                  + "<div class='post-user-name'>" + posts[0][i] + "</div>"
-                  + "<p class='post-text'>" + posts[1][i]
-                  + "</p></li>"
-              );
-            mainView.append(
-                  "<i class='fa fa-thumbs-up'></i>"
-                + "<i class='fa fa-thumb-tack'></i>"
-                + "<i class='fa fa-comment'></i>"
-            );
-        }
+    console.log(id);
+
+    $.ajax({
+        method: "POST",
+        url: "/User/GetUserInformation",
+        data: { userId: id }
     })
+   .success(function( info ) {
+       var mainView = $("#main-view");
+       mainView.empty();
+       console.log(info);
+       for (var i = info["posts"].length - 1; i >= 0; i--) {
+           mainView.append(
+                   "<li class='feed-post'>"
+                 + "<img class='post-profile-image' src='/Content/Assets/" + info["profileImage"] + ".png' />"
+                 + "<div class='post-user-name'>" + info["userName"] + "</div>"
+                 + "<p class='post-text'>" + info["posts"][i]
+                 + "</p></li>"
+             );
+           mainView.append(
+                 "<i class='fa fa-thumbs-up'></i>"
+               + "<i class='fa fa-thumb-tack'></i>"
+               + "<i class='fa fa-comment'></i>"
+           );
+       }
+   });
 }
 
 function eventsTab() {
