f6204ecd87d5db1637fc72fe43b90bafba483f35
Droken <valgeir14@ru.is>
2015-05-11 14:03:10 +0000

    more conflict

--- Bubblespace/Scripts/ajax.js
+++ Bubblespace/Scripts/ajax.js
@@ -19,17 +19,27 @@ function eventsTab() {
     })
 }
 
-$(function () {
+function newsFeed() {
     var mainView = $("#main-view");
     mainView.empty();
     $.post("/Post/GetAllPosts", function (posts) {
         console.log(posts);
-        for (var i = 0; i < posts[0].length; i++) {
+        for (var i = posts[0].length - 1; i >= 0; i--) {
             mainView.append(
                 "<li class='feed-post'>"
                   + "<img class='post-profile-image' src='/Content/Assets/" + posts[2][i] + ".png' />"
-                  + posts[0][i] + ": " + posts[1][i]
-              + "</li>");
+                  + posts[0][i] + " <p class='post-text'>" + posts[1][i]
+              + "</p></li>");
+            mainView.append(
+                  "<input class='post-content-like fa fa-thumbs-up' type='button' value='like'>"
+                + "<input class='post-content-burst' type='button' value='burst'>"
+                + "<input class='post-content-comment' type='button' value='comment'>"
+            );
         }
     })
+}
+
+$(function () {
+    friendsTab();
+    newsFeed();
 });
