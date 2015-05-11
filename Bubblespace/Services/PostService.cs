88e4c68b3184f8962e033491b12ee1f940a9febc
RoyalCookie <janusthorkristjansson@gmail.com>
2015-05-11 12:37:54 +0000

    PostService

--- Bubblespace/Services/PostService.cs
+++ Bubblespace/Services/PostService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.SqlServer.Server;
+using System.Text.RegularExpressions;
 
 namespace Bubblespace.Services
 {
@@ -68,5 +70,7 @@ namespace Bubblespace.Services
             var db = new VERK2015_H17Entities1();
             return db.posts.ToList();
         }
+
+        
     }
 }
