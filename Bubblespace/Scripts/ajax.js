56e8cbc63bca067f6a4aa65624c629e147c19749
Andri Rafn Ágústsson <andrirafna@gmail.com>
2015-05-11 15:08:19 +0000

    fontAwesome integration, icon change +ajax

--- Bubblespace/Scripts/ajax.js
+++ Bubblespace/Scripts/ajax.js
@@ -3,7 +3,33 @@
     friendslist.empty();
     $.post("/User/Friends", function (data) {
         for (var i = 0; i < data[0].length; i++) {
-            friendslist.append("<li class='list-item'><img src='/Content/Assets/" + data[1][i] + ".png'/>" + data[0][i] + "</li>");
+            friendslist.append(
+                  "<li class='list-item'>"
+                + "<img src='/Content/Assets/" + data[1][i] + ".png'/>"
+                + "<a onclick='friendMain(); return false;'>" + data[0][i] + "</a></li>"
+            );
+        }
+    })
+}
+
+function friendMain() {
+    var mainView = $("#main-view");
+    mainView.empty();
+    $.post("/Post/GetUserInformation", function (posts) {
+        // TODO: Header
+        for (var i = posts[0].length - 1; i >= 0; i--) {
+            mainView.append(
+                    "<li class='feed-post'>"
+                  + "<img class='post-profile-image' src='/Content/Assets/" + posts[2][i] + ".png' />"
+                  + "<div class='post-user-name'>" + posts[0][i] + "</div>"
+                  + "<p class='post-text'>" + posts[1][i]
+                  + "</p></li>"
+              );
+            mainView.append(
+                  "<i class='fa fa-thumbs-up'></i>"
+                + "<i class='fa fa-thumb-tack'></i>"
+                + "<i class='fa fa-comment'></i>"
+            );
         }
     })
 }
@@ -12,9 +38,12 @@ function eventsTab() {
     var eventsList = $("#list-view-items");
     eventsList.empty();
     $.post("/Event/Events", function (events) {
-        console.log(events);
         for (var i = 0; i < events[0].length; i++) {
-            eventsList.append("<li class='list-item'><img src='/Content/Assets/" + events[1][i] + ".png'/>" + events[0][i] + "</li>");
+            eventsList.append(
+                  "<li class='list-item'>"
+                + "<img src='/Content/Assets/" + events[1][i] + ".png'/>"
+                + events[0][i] + "</li>"
+            );
         }
     })
 }
@@ -26,14 +55,16 @@ function newsFeed() {
         console.log(posts);
         for (var i = posts[0].length - 1; i >= 0; i--) {
             mainView.append(
-                "<li class='feed-post'>"
+                    "<li class='feed-post'>"
                   + "<img class='post-profile-image' src='/Content/Assets/" + posts[2][i] + ".png' />"
-                  + posts[0][i] + " <p class='post-text'>" + posts[1][i]
-              + "</p></li>");
+                  + "<div class='post-user-name'>" + posts[0][i] + "</div>"
+                  + "<p class='post-text'>" + posts[1][i]
+                  + "</p></li>"
+              );
             mainView.append(
-                  "<input class='post-content-like fa fa-thumbs-up' type='button' value='like'>"
-                + "<input class='post-content-burst' type='button' value='burst'>"
-                + "<input class='post-content-comment' type='button' value='comment'>"
+                  "<i class='fa fa-thumbs-up'></i>"
+                + "<i class='fa fa-thumb-tack'></i>"
+                + "<i class='fa fa-comment'></i>"
             );
         }
     })
