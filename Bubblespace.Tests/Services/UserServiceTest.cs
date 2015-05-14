3d537fbeb7ae6a9b42441605017aa8d3a8921465
Drokens <valgeir14@ru.is>
2015-05-14 18:26:51 +0000

    Unit Test
    Kominn með 3 Unit test fyrir user service sem voru passed

--- Bubblespace.Tests/Services/UserServiceTest.cs
+++ Bubblespace.Tests/Services/UserServiceTest.cs
@@ -8,24 +8,50 @@ namespace Bubblespace.Tests.Services
     [TestClass]
     public class UserServiceTest
     {
+        [TestInitialize]
+        public void Initialize()
+        {
+            var db = new Bubblespace.VERK2015_H17Entities1();
+
+        }
+
         [TestMethod]
         public void TestGetAllFriendsForDroken()
         {
             // Arrange:
-            var db = new Bubblespace.VERK2015_H17Entities1();
-            AspNetUsers userDroken = new AspNetUsers
-            {
-                Id = "a52bb3e1-9554-4ffa-b398-b635945bcddb",
-                Email = "asdasd@asda.sa",
-                NickName = "Droken",
-                UserName = "asdasd@asda.sa"
-            };
-
+            var userDroken = UserService.GetUserByEmail("asdasd@asda.sa");
+            
             // Act:
             var friends = UserService.GetAllFriends(userDroken);
 
             // Assert:
             Assert.AreEqual(2, friends.Count);
+        }
+
+        [TestMethod]
+        public void TestGetUserPostsByDroken()
+        {
+            // Arrange:
+            var userDroken = UserService.GetUserByEmail("asdasd@asda.sa");
+
+            // Act: 
+            var posts = UserService.GetUsersPosts(userDroken);
+
+            // Assert:
+            Assert.AreEqual(2, posts.Count);
+        }
+
+        [TestMethod]
+        public void TestGetEventsCreatedByDroken()
+        {
+            // Arrange:
+            var userDroken = UserService.GetUserByEmail("asdasd@asda.sa");
+
+            // Act: 
+            var events = UserService.GetAllEventsCreatedByUser(userDroken);
+
+            // Assert:
+            Assert.AreEqual(0, events.Count);
         }
     }
 }
