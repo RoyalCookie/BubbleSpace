c50f696c9a8696a4e100d566975cf4ca5442ce3a
sveinbjorng <Sveinbjorn@SKYNET01>
2015-05-14 15:26:25 +0000

    Add auth to home controller

--- Bubblespace/Controllers/HomeController.cs
+++ Bubblespace/Controllers/HomeController.cs
@@ -17,24 +17,12 @@ namespace Bubblespace.Controllers
             }
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Bubblespace! (edit this in HomeController.cs)";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Contact Bubblespace! (edit this in HomeController.cs)";
-
-            return View();
-        }
-
         public ActionResult Home()
         {
-            ViewBag.Message = "This is the main page! (edit this in HomeController.cs)";
+            if(!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
             return View();
         }
     }
