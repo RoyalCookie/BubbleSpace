9fa4c92ab799a0018a8a0bc0a850e179c162ff9c
Drokens <valgeir14@ru.is>
2015-05-14 18:13:02 +0000

    Include Unit test project
    Búinn að fá þetta til að virka án þess að eyðileggja allt

---
+++ Bubblespace.Tests/Services/UserServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bubblespace.Services;
+using Bubblespace.Models;
+
+namespace Bubblespace.Tests.Services
+{
+    [TestClass]
+    public class UserServiceTest
+    {
+        [TestMethod]
+        public void TestGetAllFriendsForDroken()
+        {
+            // Arrange:
+            var db = new Bubblespace.VERK2015_H17Entities1();
+            AspNetUsers userDroken = new AspNetUsers
+            {
+                Id = "a52bb3e1-9554-4ffa-b398-b635945bcddb",
+                Email = "asdasd@asda.sa",
+                NickName = "Droken",
+                UserName = "asdasd@asda.sa"
+            };
+
+            // Act:
+            var friends = UserService.GetAllFriends(userDroken);
+
+            // Assert:
+            Assert.AreEqual(2, friends.Count);
+        }
+    }
+}
